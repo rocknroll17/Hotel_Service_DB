56f7ceba56df3cc96249b8f94f4a1f4840da847b
rocknroll <gon1397@naver.com>
2023-12-10 23:50:15 +0900

    static class

--- db_control.py
+++ db_control.py
@@ -3,14 +3,14 @@ class user_controller:
     password=None
     insert_user_query=None
 
-    def set_insert_query(self):
+    def set_insert_query():
         insert_user_query = "INSERT account_info (username,password) VALUES (%s,%s)"
 
-    def getUsername(self):
-        return self.username
-    def getPassword(self):
-        return self.password
-    def set_username(self,username):
-        self.username = username
-    def set_password(self,password):
-        self.password = password
+    def getUsername():
+        return user_controller.username
+    def getPassword():
+        return user_controller.password
+    def set_username(username):
+        user_controller.username = username
+    def set_password(password):
+        user_controller.password = password
