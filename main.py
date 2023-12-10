3294c8e8b422587cfc7656124f86b791c2a2c6d3
rocknroll <gon1397@naver.com>
2023-12-11 04:39:58 +0900

    Good

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import os
 import time
 
 def login():
+    os.system('cls')
+    global islogin
+    print("=== 로그인 ===")
+    if islogin:
+        print("이미 로그인 되어 있습니다.")
+        return
     login_ID = input("ID: ")
     Password = input("Password: ")
     cursor.execute("SELECT login('%s', '%s')" % (login_ID, hash_password(Password)))
@@ -24,28 +30,92 @@ JOIN customer_info c ON a.customerID = c.customerID
 WHERE a.ID = '%s' AND a.Password = '%s';""" % (login_ID, hash_password(Password)))
         result = cursor.fetchall()
         #print(result)
-        global islogin, ID, CustomerId, FirstName, LastName, Grade
+        global ID, CustomerId, FirstName, LastName, Grade
         islogin = True
         ID, CustomerId, FirstName, LastName, Grade = result[0]
         time.sleep(3)
         os.system('cls')
 
+def logout():
+    os.system('cls')
+    global islogin, ID, CustomerId, FirstName, LastName, Grade
+    islogin = False
+    ID = ''
+    CustomerId = ''
+    FirstName = ''
+    LastName = ''
+    FirstName = ''
+    Grade = ''
+    print("로그아웃 되었습니다.")
+    time.sleep(3)
+    os.system('cls')
+    return
+
+
 def add_account():
-    print("사용자 계정 추가: ")
+    os.system('cls')
+    print("=== 회원가입 ===")
+    print("회원가입: ")
     ID = (input("ID: "))
-    Password = input("Password: ")
+    Password = hash_password(input("비밀번호: "))
+    RePassword = hash_password(input("새 비밀번호 확인: "))
+    if Password != RePassword:
+        print("비밀번호가 다릅니다.")
+        time.sleep(3)
+        os.system('cls')
+        return
     FirstName = input("FirstName: ")
     LastName = input("LastName: ")
     Email = input("Email: ")
     Phone = input("Phone: ")
-    cursor.callproc('CreateAccountInfo', args=(FirstName,LastName,Email,Phone,ID,hash_password(Password)))
+    cursor.callproc('CreateAccountInfo', args=(FirstName,LastName,Email,Phone,ID,Password))
     db.commit()
+    print("회원가입이 완료 되었습니다. 되었습니다.")
+    time.sleep(3)
+    os.system('cls')
+    return
+    
+def change_password():
+    os.system('cls')
+    print("=== 비밀번호 변경 ===")
+    if not islogin:
+        print("로그인을 해야 이용할 수 있는 서비스입니다.")
+        return
+    print("비밀번호 변경")
+    OldPassword = hash_password(input("기존 비밀번호: "))
+    cursor.execute("SELECT check_user_existence('%s', '%s')"%(ID, OldPassword))
+    if cursor.fetchone()[0]:
+        NewPassword = hash_password(input("새 비밀번호: "))
+        RePassword = hash_password(input("새 비밀번호 확인: "))
+        if NewPassword == RePassword:
+            cursor.execute("UPDATE account_info SET Password = '%s' WHERE ID = '%s'"%(NewPassword, ID))
+            db.commit()
+        else:
+            print("비밀번호가 다릅니다.")
+            time.sleep(3)
+            os.system('cls')
+            return
+    else:
+        print("비밀번호가 틀렸습니다.")
+        time.sleep(3)
+        os.system('cls')
+        return
 
 def resign():
-    ID = input("ID: ")
-    Password = input("Password: ")
-    cursor.execute("UPDATE account_info SET Active = 0 WHERE ID='%s' AND Password = '%s'"%(ID, hash_password(Password)))
+    os.system('cls')
+    print("=== 회원 탈퇴 ===")
+    OldPassword = hash_password(input("탈퇴를 하려면 비밀번호를 입력하세요: "))
+    cursor.execute("SELECT check_user_existence('%s', '%s')"%(ID, OldPassword))
+    if cursor.fetchone()[0]:
+        cursor.execute("UPDATE account_info SET Active = 0 WHERE ID='%s' AND Password = '%s'"%(ID, OldPassword))
+    else:
+        print("회원 정보가 잘못 되었습니다.")
+        return
     db.commit()
+    print("회원 탈퇴되어 자동 로그아웃 됩니다. 한달 안에 다시 로그인 시 탈퇴가 취소됩니다.")
+    time.sleep(3)
+    os.system('cls')
+    logout()
 
 def hash_password(password):
     # 무작위 솔트 생성
@@ -55,11 +125,18 @@ def hash_password(password):
     return hash.hexdigest()
 
 def reservation():
+    os.system('cls')
+    print("=== 객실 예약 ===")
     if not islogin:
         print("로그인을 해야 이용할 수 있는 서비스입니다.")
+        time.sleep(3)
+        os.system('cls')
         return
+    
+    #여기에 코드 넣어서 해결
 
 
+    os.system('cls')
 
 # Create an instance of DatabaseAuthInformation
 db_info = DatabaseAuthInformation()
@@ -98,30 +175,48 @@ LastName = ''
 FirstName = ''
 Grade = ''
 while True:
-    if islogin:
+    if not islogin:
+        print("=== Main Menu ===")
+        print("1. 로그인")
+        print("2. 회원가입")
+        print("3. 종료")
+        choice = input("메뉴를 선택하세요: ")
+        if choice == "1":
+            login()
+        elif choice == "2":
+            add_account()
+        elif choice == "3":
+            print("프로그램을 종료합니다.")
+            break
+        else:
+            print("올바른 메뉴를 선택하세요.")
+    else:
         print("사용자: %s %s"%(FirstName, LastName))
         print("ID: %s"%(ID))
-    print("=== Main Menu ===")
-    print("1. 로그인")
-    print("2. 사용자 계정 추가")
-    print("3. 사용자 계정 탈퇴")
-    print("4. 객실 예약")
-    print("5. 종료")
+        print("=== Main Menu ===")
+        print("1. 객실 예약")
+        print("2. 비밀번호 변경")
+        print("3. 탈퇴")
+        print("4. 로그아웃")
+        print("5. 종료")
+        choice = input("메뉴를 선택하세요: ")
         
-    choice = input("메뉴를 선택하세요: ")
-    if choice == "1":
-        login()
-    elif choice == "2":
-        add_account()
-    elif choice == "3":
-        resign()
-    elif choice == "4":
-        reservation()
-    elif choice == "5":
-        print("프로그램을 종료합니다.")
-        break
-    else:
-         print("올바른 메뉴를 선택하세요.")
+        
+        if choice == "1":
+            reservation()
+        elif choice == "2":
+            change_password()
+        elif choice == "3":
+            resign()
+        elif choice == "4":
+            logout()
+        elif choice == "5":
+            print("프로그램을 종료합니다.")
+            break
+        else:
+            print("올바른 메뉴를 선택하세요.")
+            time.sleep(3)
+            os.system('cls')
 
 # 결과 출력
 
